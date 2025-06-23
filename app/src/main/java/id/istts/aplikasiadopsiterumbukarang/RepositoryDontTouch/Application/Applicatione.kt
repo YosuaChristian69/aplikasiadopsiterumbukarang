a7f45cf5e91fba29e56ab9022e33121deafe2117
Ken <ken3lliot@gmail.com>
2025-06-23 09:20:44 +0700

    initialized repository folder/package
    folder / package RepositoryDontTouch gak boleh diotak-atik, diubah, disentuh... kalo mau coba2 atau belajar repository buat folder sendiri atau boleh juga ini dicopy terus coba2 di copy an nya itu...

---
+++ app/src/main/java/id/istts/aplikasiadopsiterumbukarang/RepositoryDontTouch/Application/Applicatione.kt
@@ -0,0 +1,21 @@
+package id.istts.aplikasiadopsiterumbukarang.RepositoryDontTouch.Application
+
+import android.app.Application
+import retrofit2.Retrofit
+
+class Applicatione {
+    companion object{
+//        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+//        val retrofit = Retrofit.Builder().addConverterFactory(
+//            MoshiConverterFactory.create(moshi)
+//        ).baseUrl("http://192.168.18.35:3000").build()
+//        val retrofitService = retrofit.create(webService::class.java)
+//        lateinit var db: AppDatabase
+
+    }
+
+//    override fun onCreate() {
+//        super.onCreate()
+//        db = AppDatabase.getInstance(this)
+//    }
+}
