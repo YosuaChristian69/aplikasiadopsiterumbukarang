8ad662131f1456515da41335c173e65cfdcc7d1f
Ricardhad <134259818+Ricardhad@users.noreply.github.com>
2025-06-29 17:19:55 +0700

    berubah url biar gk bingung

--- app/src/main/java/id/istts/aplikasiadopsiterumbukarang/service/RetrofitClient.kt
+++ app/src/main/java/id/istts/aplikasiadopsiterumbukarang/service/RetrofitClient.kt
@@ -7,8 +7,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 //    private const val BASE_URL = "http://10.101.17.208:3000"
-//    private const val BASE_URL = "http://10.0.2.2:3000"
-    private const val BASE_URL = "http://192.168.1.102:3000/"
+    private const val BASE_URL = "http://10.0.2.2:3000"
+//    private const val BASE_URL = "http://192.168.1.102:3000/"
 
     // 1. Create the logging interceptor to see network traffic
 //    private val loggingInterceptor = HttpLoggingInterceptor().apply {
